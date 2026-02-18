b9fbeeb9285c2da0d127c2851a9cb0a64c4405f1
Michael Shonov <waveafterwave3w2@gmail.com>
2026-02-18 14:48:06 +0300

    feat: use wasd-eslint package for eslint

--- client/eslint.config.ts
+++ client/eslint.config.ts
@@ -1,33 +1,3 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
-import pluginVue from 'eslint-plugin-vue'
+import ESLintConfig from 'wasdwave-eslint'
 
-export default [
-    {
-        files: ['**/*.{js,mjs,cjs,ts,mts,cts,vue}'],
-        languageOptions: {
-            globals: {
-                ...globals.browser,
-            },
-        },
-        rules: {
-            'no-console': 'warn',
-            'no-else-return': 'warn',
-            eqeqeq: ['error', 'always'],
-            'no-var': 'error',
-            'prefer-const': 'error',
-        },
-    },
-    js.configs.recommended,
-    ...tseslint.configs.recommended,
-    ...pluginVue.configs['flat/essential'],
-    {
-        files: ['**/*.vue'],
-        languageOptions: {
-            parserOptions: {
-                parser: tseslint.parser,
-            },
-        },
-    },
-]
+export default [...ESLintConfig]
